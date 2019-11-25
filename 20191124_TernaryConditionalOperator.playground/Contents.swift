4dc587e854dea585298c46fb74507362fe027d6c
Jason Mitchell <devjmitchell@gmail.com>
2019-11-24 22:26:32 -0700

    Add ternary conditional operator playground

---
+++ 20191124_TernaryConditionalOperator.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let age = 19
+var message = ""
+
+//if age >= 21 {
+//    message = "You're old enough to drink!"
+//} else {
+//    message = "You're underage... I hear rootbeer is good!"
+//}
+
+// what ? true : false
+message = age >= 21 ? "You're old enough to drink!" : "You're underage... I hear rootbeer is good!"
+
+print(message)
