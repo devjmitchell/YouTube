df594d9c1ce8c5f65b1c022936e4cb078a76b401
Jason Mitchell <devjmitchell@gmail.com>
2019-11-24 23:11:24 -0700

    Add basic operators playground

---
+++ 20191125_BasicOperators.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+7 + 3
+
+let firstNumber = 7
+let secondNumber = 3
+
+firstNumber + secondNumber
+firstNumber - secondNumber
+firstNumber * secondNumber
+firstNumber / secondNumber
+
+firstNumber % secondNumber
+
+var a = 6
+
+a += 2
+a -= 2
+a *= 2
+a /= 2
+
+3 == 2
+3 != 2
+3 > 2
+3 >= 2
+3 < 2
+3 <= 2
+
+true || false
+true && false
+
+let bool1 = true
+let bool2 = false
+
+if bool1 || bool2 {
+    print("Success!")
+} else {
+    print("Failed!")
+}
+
+let one = "one"
+let two = " two"
+
+one + two
+
+let like = "Like"
+let sub = " and subscribe!"
+
+print(like + sub)
